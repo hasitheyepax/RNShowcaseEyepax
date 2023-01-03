d5610b745739131878de666cefe69c94c1159eac
Hasith Methmal Jayasekara <hasith.m@eyepax.com>
2023-01-03 16:02:11 +0530

    [chore] Auth slice added

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -4,8 +4,11 @@ import {
   Action,
   combineReducers,
 } from "@reduxjs/toolkit";
+import authSlice from "./slices/authSlice";
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  auth: authSlice,
+});
 
 export const store = configureStore({
   reducer: rootReducer,
