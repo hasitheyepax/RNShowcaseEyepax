ca48fad8413e0f5c17219a61833563f575bdebd1
IsuruRanapana <113498635+IsuruRanapana@users.noreply.github.com>
2023-01-04 13:15:03 +0530

    [feat]: Bottom Navigation bar added

---
+++ src/components/TabBarComponent.tsx
@@ -0,0 +1,97 @@
+import { BottomTabNavigationOptions } from "@react-navigation/bottom-tabs";
+import { useEffect, useRef } from "react";
+import {
+  LayoutChangeEvent,
+  Pressable,
+  StyleSheet,
+  View,
+  Text,
+} from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  withTiming,
+} from "react-native-reanimated";
+
+type TabBarComponentProps = {
+  active?: boolean;
+  options: BottomTabNavigationOptions;
+  onLayout: (e: LayoutChangeEvent) => void;
+  onPress: () => void;
+};
+
+export default function TabBarComponent({
+  active,
+  options,
+  onLayout,
+  onPress,
+}: TabBarComponentProps) {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    if (active && ref?.current) {
+      //@ts-ignore
+      ref.current.play();
+    }
+  }, [active]);
+
+  const animatedComponentCircleStyles = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          scale: withTiming(active ? 1 : 0, { duration: 250 }),
+        },
+      ],
+    };
+  });
+
+  const animatedIconContainerStyles = useAnimatedStyle(() => {
+    return {
+      opacity: withTiming(active ? 1 : 0.5, { duration: 250 }),
+    };
+  });
+
+  return (
+    <Pressable onPress={onPress} onLayout={onLayout} style={styles.container}>
+      <Animated.View
+        style={[styles.componentCircle, animatedComponentCircleStyles]}
+      />
+      <Animated.View
+        style={[styles.iconContainer, animatedIconContainerStyles]}
+      >
+        {options.tabBarIcon ? (
+          /* @ts-ignore */
+          options.tabBarIcon({ ref })
+        ) : (
+          <Text style={styles.text}>?</Text>
+        )}
+      </Animated.View>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: 60,
+    width: 60,
+    marginTop: -5,
+  },
+  text: {
+    color: "#000",
+    fontSize: 18,
+    textAlign: "center",
+  },
+  componentCircle: {
+    flex: 1,
+    borderRadius: 30,
+    backgroundColor: "white",
+  },
+  iconContainer: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
--- src/navigation/User.tsx
+++ src/navigation/User.tsx
@@ -1,19 +1,90 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Navigation } from "../config";
-import Screens from "../screens";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Lottie from "lottie-react-native";
+import { StyleSheet } from "react-native";
+import AnimatedTabBar from "../components/AnimatedTabBar";
+import homeIcon from "../components/assets/icons/home.icon.json";
+import profileIcon from "../components/assets/icons/profile.icon.json";
+import searchIcon from "../components/assets/icons/search.icon.json";
+import settingsIcon from "../components/assets/icons/settings.icon.json";
+import Home from "../screens/Home";
+import Profile from "../screens/Profile";
+import Search from "../screens/Search";
+import Settings from "../screens/Settings";
 
-const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
 
 const User = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name={Navigation.home}
-        component={Screens.Home}
-        options={{ headerShown: false }}
+    <BottomTabs.Navigator tabBar={(props) => <AnimatedTabBar {...props} />}>
+      <BottomTabs.Screen
+        options={{
+          //@ts-ignore
+          tabBarIcon: ({ ref }) => (
+            <Lottie
+              ref={ref}
+              loop={false}
+              style={styles.icon}
+              source={homeIcon}
+            />
+          ),
+        }}
+        name="Home"
+        component={Home}
       />
-    </Stack.Navigator>
+      <BottomTabs.Screen
+        options={{
+          //@ts-ignore
+          tabBarIcon: ({ ref }) => (
+            <Lottie
+              ref={ref}
+              loop={false}
+              style={styles.icon}
+              source={searchIcon}
+            />
+          ),
+        }}
+        name="Chat"
+        component={Search}
+      />
+      <BottomTabs.Screen
+        options={{
+          //@ts-ignore
+          tabBarIcon: ({ ref }) => (
+            <Lottie
+              ref={ref}
+              loop={false}
+              style={styles.icon}
+              source={profileIcon}
+            />
+          ),
+        }}
+        name="Upload"
+        component={Profile}
+      />
+      <BottomTabs.Screen
+        options={{
+          //@ts-ignore
+          tabBarIcon: ({ ref }) => (
+            <Lottie
+              ref={ref}
+              loop={false}
+              style={styles.icon}
+              source={settingsIcon}
+            />
+          ),
+        }}
+        name="Settings"
+        component={Settings}
+      />
+    </BottomTabs.Navigator>
   );
 };
+
+const styles = StyleSheet.create({
+  icon: {
+    height: 32,
+    width: 32,
+  },
+});
 
 export default User;
