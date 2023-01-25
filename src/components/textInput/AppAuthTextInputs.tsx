f5757bd3786f76f6121b04cf2e2aa3c91e950274
IsuruRanapana <113498635+IsuruRanapana@users.noreply.github.com>
2023-01-25 12:49:50 +0530

    [feat]: created a common auth input component and added form validations to the login screen.

---
+++ src/components/textInput/AppAuthTextInputs.tsx
@@ -0,0 +1,74 @@
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TextInputProps,
+  View,
+  Dimensions,
+} from "react-native";
+import React from "react";
+import { BlurView } from "expo-blur";
+
+interface AppAuthTextInputProps extends TextInputProps {
+  error?: string;
+}
+
+const AppAuthTextInput: React.FC<AppAuthTextInputProps> = (props) => {
+  const { error } = props;
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.labelContainer}>
+        <Text style={styles.errorText}>{error}</Text>
+      </View>
+      <View style={styles.blurViewContainer}>
+        <BlurView intensity={2} style={styles.textInputContainer}>
+          <TextInput style={styles.textInput} {...props} />
+        </BlurView>
+      </View>
+    </View>
+  );
+};
+
+export default AppAuthTextInput;
+const { width, height } = Dimensions.get("window");
+
+const styles = StyleSheet.create({
+  container: {
+    width: 0.95 * width,
+    borderRadius: 10,
+  },
+  labelContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    marginLeft: 0.025 * width,
+    top: 0.006 * height,
+  },
+  labelText: {
+    fontSize: 16,
+    fontWeight: "700",
+    color: "white",
+  },
+  errorText: {
+    fontSize: 12,
+    fontWeight: "300",
+    color: "red",
+  },
+  textInputContainer: {
+    width: 0.9 * width,
+    height: 0.06 * height,
+    justifyContent: "center",
+    padding: 0.03 * width,
+    borderRadius: 6,
+  },
+  blurViewContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 0.008 * height,
+  },
+  textInput: {
+    color: "#FFFFFF",
+    fontSize: 16,
+    fontWeight: "400",
+  },
+});
